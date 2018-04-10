62cf63891752b3f8d4dd2dfc6fb6d5fe175c05a0
James <layer_james@163.com>
2018-04-10 21:54:36 +0800

    Initial Commit

---
+++ BlockDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BlockDemo
+//
+//  Created by 冷健 on 2018/4/10.
+//  Copyright © 2018年 冷健. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ BlockDemo/ViewController.m
@@ -0,0 +1,44 @@
+//
+//  ViewController.m
+//  BlockDemo
+//
+//  Created by 冷健 on 2018/4/10.
+//  Copyright © 2018年 冷健. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    [self globalBlock];
+    
+}
+
+/**
+ 全局block
+ */
+- (void)globalBlock {
+    //定义一个block
+    void (^testBlock)(void) = ^() {
+        
+    };
+    /*
+     打印testBlock
+     2018-04-10 21:40:01.488588+0800 BlockDemo[22221:9676621] <__NSGlobalBlock__: 0x105d03088>
+     */
+    NSLog(@"%@",testBlock);
+}
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
